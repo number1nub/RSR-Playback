572a3587fd0e32aab01959f704daecff4f990202
number1nub <number1nub@gmail.com>
2020-08-22 15:24:47 -0500

    - Adding supplemental files

---
+++ RSR Playback.bat
@@ -0,0 +1,5 @@
+rem start "C:\Program Files (x86)\TerraVici Drilling Solutions\CS-SS-RSR Utility 9.1.3\CS-SS-RSR Utility 9.1.3.exe"
+
+CD "C:\Program Files (x86)\BEL\RealTerm"
+
+realterm.exe baud=9600 port=6 sendfile="C:\Users\rameen25365\Desktop\GVSequence.txt" linedly=500 CR=1 LF=1
